008ebaa28c13233f4570da0f56607fedb21ee070
“Evan <frommere@uchicago.edu>
2022-04-12 14:11:36 -0400

    populate method

--- Memory_Puzzle/board.rb
+++ Memory_Puzzle/board.rb
@@ -3,20 +3,21 @@ require "byebug"
 class Board
   LETTERS = ("A".."Z").to_a
   
-  def initialize(n = 3)
+  def initialize(n = 4)
     @size = n * n
     @grid = Array.new(n) {Array.new(n)}
   end
 
   def populate
-    debugger
     gridletters = (LETTERS.sample(@size / 2) * 2).shuffle
     i = 0
-    @grid.map! do |row|
-      row.map! do |ele|
-        gridletters[i]
+    debugger
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |ele, ele_index|
+        @grid[row_index][ele_index] = gridletters[i]
         i += 1
       end
+      # @grid[row_idx] = new_row
     end
   end
 end
