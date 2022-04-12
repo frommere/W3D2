80b009c18b83496c445d87455d175302f864c824
Leo Chung <leo.chung97@gmail.com>
2022-04-12 16:01:36 -0400

    Attempting AI 2

--- Memory_Puzzle/computer.rb
+++ Memory_Puzzle/computer.rb
@@ -1,13 +1,25 @@
 require_relative "game.rb"
 class Computer
-    attr_reader :name
-    
-    def initialize
-        @name = "computer"
-    end
+  attr_reader :name
+  
+  def initialize
+    @name = "computer"
+    @knowncards = {}
+    @matched_positions = []
+  end
 
-    def get_position(size)
-        response = [rand(0...size), rand(0...size)]
-        response
-    end
+  def get_position(size)
+    response = [rand(0...size), rand(0...size)]
+    response
+  end
+
+  def receive_revealed_card(pos, value)
+    @knowncards[value] = pos
+  end
+
+  def receive_match(pos_1, pos_2)
+    @matched_positions.push(pos_1)
+    @matched_positions.push(pos_2)
+  end
+
 end
